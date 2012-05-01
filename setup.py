ad9ac165c2090d8496aa671ccac76e97745dac42
Espen Kåsa Notodden <enotodden@gmail.com>
2012-05-01 21:21:39 +0200

    all scripts now executes the 'vu' main script

--- setup.py
+++ setup.py
@@ -2,24 +2,25 @@
 from distutils.core import setup
 
 setup(
-	name			=	"vimutils",
-	version			=	"0.2",
-	description		=	"Vim utilities..",
-	author			=	"Espen Notodden",
-	author_email	=	"enotodden@gmail.com",
-	url				=	"http://github.com/enotodden/vimutils",
-	packages		=	[
-							'vimutils'
-						],
-	scripts			=	[
-							"vt",
-							"ve",
-							"vs",
-							"vls",
-                            "vbls",
-                            "vdb",
-                            "vds"
-						]
+    name="vimutils",
+    version="0.2",
+    description="Vim utilities..",
+    author="Espen Notodden",
+    author_email="enotodden@gmail.com",
+    url="http://github.com/enotodden/vimutils",
+    packages=[
+        'vimutils'
+    ],
+    scripts=[
+        "scripts/vt",
+        "scripts/ve",
+        "scripts/vs",
+        "scripts/vls",
+        "scripts/vbls",
+        "scripts/vdb",
+        "scripts/vds",
+        "scripts/vu"
+    ]
 )
 
 
--- vimutils/__init__.py
+++ vimutils/__init__.py
@@ -4,36 +4,48 @@ import subprocess
 import json
 import re
 
+
+def readfile(filename):
+    f = open(filename, "r")
+    c = f.read()
+    f.close()
+    return c
+
+def writefile(filename, contents):
+    f = open(filename, "w")
+    f.write(contents)
+    f.close()
+
 VIMUTILS_DEFAULT_RC = os.path.join(os.environ["HOME"], ".vimutilsrc")
 
 class VimUtils(object):
 
-    def __init__(self,rcfile=VIMUTILS_DEFAULT_RC):
+    def __init__(self,servername=None,rcfile=None):
+        if rcfile == None:
+            rcfile = VIMUTILS_DEFAULT_RC
+
         self.rcfile = rcfile
-        f = open(rcfile,"r")
-        self.rc = json.loads(f.read())
-        f.close()
-
-
+        self.rc = json.loads(readfile(self.rcfile))
+        self.vim = self.rc["vim_cmd"]
+        self.server = self.rc["default_server"]
+        if servername != None:
+            self.server = servername
+    
     def write_rc(self):
-        f = open(self.rcfile,"w")
-        f.write(json.dumps(self.rc))
-        f.close()
+        writefile(self.rcfile,json.dumps(self.rc))
 
     def servers(self):
         try:
-            result = subprocess.check_output([self.rc["vim_cmd"], "--serverlist"]).strip().split("\n")
+            result = subprocess.check_output([self.vim, "--serverlist"]).strip().split("\n")
             return result
         except:
             return None
 
-    def start_server(self, servername=None):
-        if servername == None:
-            servername = self.rc["default_server"]
-        if servername in self.servers():
+    def start(self):
+        if self.server in self.servers():
             return False
         else:
-            cmdstr = "%s --servername %s" % (self.rc["vim_cmd"], servername)
+            cmdstr = "%s --servername %s" % (self.vim, self.server)
             return 0 == os.system(cmdstr)
 
 
@@ -43,52 +55,43 @@ class VimUtils(object):
         else:
             return False
 
-    def run_command(self,command,servername=None):
-        if servername == None:
-            servername = self.rc["default_server"]
-    
+    def run_command(self,command):
         return subprocess.check_output([
-                self.rc["vim_cmd"],
+                self.vim,
                 "--servername",
-                servername,
+                self.server,
                 "--remote-expr",
                 "VimUtilsCommandOutput(\"%s\")" % command])
-    
 
-    def open_files(self, files, servername=None):
-        if servername == None:
-            servername = self.rc["default_server"]
+
+    string = "Hello shithead"
+    liste = ["hello", "marting", "something", "something"]
+    def open_files(self, files):
         for f in files:
             f = os.path.realpath(f)
-            cmds = [self.rc["vim_cmd"], "--servername", servername, "--remote-send", "<ESC>:e %s <CR>" % (f)]
+            cmds = [self.vim, "--servername", self.server, "--remote-send", "<ESC>:e %s <CR>" % (f)]
             if subprocess.call(cmds) != 0:
                 raise Exception
         return True
 
 
-    def open_tabs(self, files, servername=None):
-        if servername == None:
-            servername = self.rc["default_server"]
-        cmdstr = "%s --servername %s --remote-tab-silent " % (self.rc["vim_cmd"], servername)
+    def open_tabs(self, files):
+        cmdstr = "%s --servername %s --remote-tab-silent " % (self.vim, self.server)
         for f in files:
             cmdstr += " " + f + " "
         if os.system(cmdstr) != 0:
             raise Exception
 
 
-    def buffers(self, servername=None):
-        if servername == None:
-            servername = self.rc["default_server"]
+    def buffers(self):
         return subprocess.check_output([
-                self.rc["vim_cmd"],"--servername", servername, "--remote-expr", "VimUtilsCommandOutput(\":buffers\")"]) 
+                self.vim,"--servername", self.server, "--remote-expr", "VimUtilsCommandOutput(\":buffers\")"])
 
 
 
-    def buffers_object(self,servername=None):
-        if servername == None:
-            servername = self.rc["default_server"]
-        outp = self.run_command("buffers", servername)
-        
+    def buffers_object(self):
+        outp = self.run_command("buffers")
+
         lines = outp.strip().split("\n")
         bufs = []
 
@@ -137,37 +140,29 @@ class VimUtils(object):
                     continue
 
             bufs.append(buf)
-            
+
         return bufs
 
 
 
-    def dump_buffer(self, bufno, servername=None):
-        if servername == None:
-            servername = self.rc["default_server"]
+    def dump_buffer(self, bufno):
         filename = subprocess.check_output([
-                    self.rc["vim_cmd"],
+                    self.vim,
                     "--servername",
-                    servername,
+                    self.server,
                     "--remote-expr",
                     "VimUtilsWriteBufferToTmpFile(\"%s\")" % (str(bufno))
                 ]).strip()
-        f = open(filename, "r")
-        contents = f.read()
-        f.close()
+        contents = readfile(filename)
         return contents
-            
 
 
-    def get_session(self,servername=None):
-        if servername == None:
-            servername = self.rc["default_server"]
+
+    def get_session(self):
         out = []
-        for buf in self.buffers_object(servername):
+        for buf in self.buffers_object():
             buf["content"] = self.dump_buffer(buf["id"])
             out.append(buf)
         return out
-            
-
 
 
